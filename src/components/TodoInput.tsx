1e74c2c3234f1c00348825862f3b01e3c30178d2
klihonga11 <klihonga11@gmail.com>
2025-12-09 04:23:50 +0200

    Added 'add' button

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 
-function TodoInput() {
-    const [todoItem,setTodoItem] = useState("");
+type TodoInputProps = {
+    onAdd: (item:string) => void 
+}
+
+function TodoInput({onAdd}: TodoInputProps) {
+    const [todoItem,setTodoItem] = useState<string>("");
+
+    function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        onAdd(todoItem);
+        setTodoItem("");
+    }
 
     return (
         <>
-            <label htmlFor="item">Item: </label>
-            <input id="item" type="text" value={todoItem} onChange={(e) => setTodoItem(e.target.value)}/>
+            <form onSubmit={onSubmit} >
+                <label htmlFor="item">Item: </label>
+                <input id="item" type="text" value={todoItem} onChange={(e) => setTodoItem(e.target.value)}/>
+                <button type="submit">Add</button>
+            </form>
         </>
     )
 }
