8a0ca6f34f353672b5332e0b0bcf1232b61d2516
klihonga11 <klihonga11@gmail.com>
2025-12-11 03:31:35 +0200

    edit list item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type TodoListProps = {
     listItems: string[]
 }
@@ -6,10 +8,33 @@ function TodoList({ listItems }: TodoListProps) {
     return(
         <>
             <p>List of items: </p>
-            <ul>
-               {listItems.map((item,index) => <li key={index}>{item}</li>)}
-            </ul>
+            {listItems.map((item,index) => <ListItem key={index} item={item}/>)}
         </>
+    )
+}
+
+type ListItemProps = {
+    item: string
+}
+
+function ListItem({item}: ListItemProps) {
+    const [editMode,setEditMode] = useState<boolean>(false);
+    const [editItem, setEditItem] = useState<string>(item);
+
+    if(editMode) {
+        return (
+            <div>
+                <input id="inputEdit" value={editItem} onChange={(e) => setEditItem(e.target.value)}></input>
+                <button onClick={() => setEditMode(false)}>Save</button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <span>{editItem}</span>
+            <button onClick={() => setEditMode(true)}>Edit</button>
+        </div>
     )
 }
 
