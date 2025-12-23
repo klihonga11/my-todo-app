6f5c9253c40372dd779ac12b66b0046c6a614c11
klihonga11 <klihonga11@gmail.com>
2025-12-23 04:07:02 +0200

    Migrate functionality to custom hook

---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+import type { TodoItem } from "../components/types/TodoItem";
+
+export function useTodos() {
+    const [todos, setTodos] = useState<TodoItem[]>([]);
+    const idCounter = useRef(0);
+    const [filter, setFilter] = useState("All");
+
+    const addTodo = (item:string) => {
+        setTodos([...todos, { id: idCounter.current, text: item, selected: false }]);
+        idCounter.current++;
+    }
+
+    const editTodo = (index: number, item:string) => {
+        const newList = [...todos];
+        const itemAtIndex = newList[index];
+        itemAtIndex.text = item;
+        newList[index] = itemAtIndex;
+        setTodos(newList);
+    }
+
+    const deleteTodo = (index:number) => {
+        const newList = [...todos]
+        newList.splice(index, 1)
+        setTodos(newList)
+    }
+
+    const selectTodo = (index:number) => {
+        const newList = [...todos];
+        const itemAtSelectedIndex = newList[index];
+        newList[index].selected = !itemAtSelectedIndex.selected;
+        setTodos(newList);
+    }
+
+    const updateFilter = (value: string) => {
+        setFilter(value);
+    }
+
+    const filteredList = filter === "All" ? todos : [...todos].filter(item => filter === "Completed" ? item.selected : !item.selected);
+
+    return {
+        addTodo,
+        editTodo,
+        deleteTodo,
+        selectTodo,
+        updateFilter,
+        filteredList
+    }
+}
