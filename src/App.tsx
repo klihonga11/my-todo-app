f532be0b795f74d1bc7f5524202b1acac6633239
klihonga11 <klihonga11@gmail.com>
2025-12-27 06:25:26 +0200

    setup chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import TodoHome from './components/TodoHome'
+import { Provider } from './components/ui/provider'
 
 function App() {
   return (
-    <>
+    <Provider>
       <h1>Todo app</h1>
       <TodoHome></TodoHome>
-    </>
+    </Provider>
   )
 }
 
